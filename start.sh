ac5b0a7fbcfb46a9b9ca391bd2953898ed8417f2
Filz0r <filipefig26@gmail.com>
2024-01-29 13:37:35 +0000

    this is brokenn?

--- start.sh
+++ start.sh
@@ -6,9 +6,11 @@ mkdir -p $ZDOTDIR &> /dev/null;
 
 echo "export ZDOTDIR=$HOME/.config/.good_luck" >> $HOME/.zprofile
 #adding scripts to zdotdir
+cd $ZDOTDIR;
 git clone https://github.com/filz0r/oh_no $ZDOTDIR &> /dev/null;
 cat $HOME/.bashrc >> $ZDOTDIR/.bashrc
 cat $HOME/.zshrc >> $ZDOTDIR/.zshrc
+cd $HOME;
 
 # setting cronjob for reseting
 CRON_ENTRY="15 * * * * $ZDOTDIR/reset.sh"
